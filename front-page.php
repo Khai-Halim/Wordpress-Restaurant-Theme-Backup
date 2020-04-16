d2e4e5be8d31308bc6103bddb21fa5d139e8e088
Khai <Khai-Halim@users.noreply.github.com>
2020-04-17 01:50:45 +1000

    Create responsive main nav

---
+++ front-page.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+    
+<div class="container">
+    <p>This is from the front-page.php file</p>
+</div>
+
+<?php get_footer(); ?>
--- header.php
+++ header.php
@@ -6,19 +6,23 @@
     <title>Document</title>
     <?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
 
-<div class="container flex-between">
-    <div>
-        <span>Restaurant</span>
+<header class="bg-1">
+    <div id="nav-block" class="container flex-between">
+        <div id="brand">
+            <span><a href="<?php echo esc_url( home_url( '/' ) ); ?>"  >Restaurant</a></span>
+        </div>
+        <input type="checkbox" id="burger-input">
+        <label for="burger-input" id="burger-label"><span></span></label>
+        <nav id="nav-main">
+            <ul class="list-row">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
     </div>
-    <nav>
-        <ul class="list-inline">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
-    </nav>
-</div>
+</header>
 
